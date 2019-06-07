c84976a0d049490d22c10c45f160c308432ab753
vicanso <vicansocanbico@gmail.com>
2019-06-07 11:15:55 +0800

    test: update test

--- recover_test.go
+++ recover_test.go
@@ -1,14 +1,18 @@
 package recover
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/vicanso/cod"
 )
 
 func TestRecover(t *testing.T) {
+	assert := assert.New(t)
 	var ctx *cod.Context
 	d := cod.New()
 	d.Use(New())
@@ -34,15 +38,28 @@ func TestRecover(t *testing.T) {
 	})
 
 	d.ServeHTTP(resp, req)
-	if resp.Code != http.StatusInternalServerError ||
-		resp.Body.String() != "category=cod-recover, message=abc" ||
-		!ctx.Committed ||
-		!catchError {
-		t.Fatalf("recover fail")
-	}
+	assert.Equal(resp.Code, http.StatusInternalServerError)
+	assert.Equal(resp.Body.String(), "category=cod-recover, message=abc")
+	assert.True(ctx.Committed)
+	assert.True(catchError)
 	for _, key := range keys {
-		if resp.Header().Get(key) != "" {
-			t.Fatalf("reset response header fail")
+		assert.Empty(ctx.GetHeader(key), "header should be reseted")
+	}
+}
+
+// https://stackoverflow.com/questions/50120427/fail-unit-tests-if-coverage-is-below-certain-percentage
+func TestMain(m *testing.M) {
+	// call flag.Parse() here if TestMain uses flags
+	rc := m.Run()
+
+	// rc 0 means we've passed,
+	// and CoverMode will be non empty if run with -cover
+	if rc == 0 && testing.CoverMode() != "" {
+		c := testing.Coverage()
+		if c < 0.9 {
+			fmt.Println("Tests passed but coverage failed at", c)
+			rc = -1
 		}
 	}
+	os.Exit(rc)
 }
